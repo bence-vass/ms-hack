aefe1e1a6b8be2736aeb87f72483d7b1551a00af
Bence Vass <v.bence.vass@gmail.com>
2023-11-29 20:01:12 +0100

    web fix

--- app/(quiz)/quiz/page.js
+++ app/(quiz)/quiz/page.js
@@ -3,46 +3,13 @@ import React from 'react';
 import {Button, Checkbox, Col, InputNumber, Radio, Row, Steps} from "antd";
 import {useAppDispatch, useAppSelector} from "@/redux/hooks";
 import {setCurrentQuestion} from "@/redux/features/quiz/quizSlice";
+import QuestionSwitch from "@/ui/question-switch";
+import {quiz} from "@/app/(quiz)/quiz/dummy_quiz";
 
 
 function Page(props) {
 
 
-    const quiz = [
-        {
-            type: 'true_or_false',
-            question: '1 + 1 = 3',
-            correct: false,
-            hint: 'its not',
-            desc: 'yes or no',
-        },
-        {
-            type: 'multiple_choice',
-            question: 'f(x) = 2*x and x = 4',
-            answers: ['2', '4', '6', '8', '10'],
-            correct: 4,
-            hint: '2 * 4',
-            desc: 'multiple choice',
-
-        },
-        {
-            type: 'numeric',
-            question: '5x and x = 10',
-            correct: 50,
-            desc: 'analysis',
-
-        },
-        {
-            type: 'checking',
-            question: 'x^2 = 4',
-            answers: ['-1', '-2', '-3', '2'],
-            correct: [1, 3],
-            desc: 'guessing',
-            status: 'error'
-
-        }
-
-    ]
 
     let stepsItems = quiz.map((e, i) => {
         return {
@@ -87,54 +54,6 @@ function Page(props) {
     );
 }
 
-
-function QuestionSwitch({question}) {
-    function questionOptions(question) {
-        switch (question.type) {
-            case 'true_or_false':
-                return (
-                    <Radio.Group>
-                        <Radio value={true}>True</Radio>
-                        <Radio value={false}>False</Radio>
-                    </Radio.Group>
-                )
-            case 'multiple_choice':
-                return (
-                    <Radio.Group>
-                        {question.answers.map((e, i) => {
-                            return (
-                                <Radio value={i} key={i}>{e}</Radio>
-                            )
-                        })}
-                    </Radio.Group>
-                )
-            case 'numeric':
-                return (
-                    <InputNumber size={"large"}/>
-                )
-            case 'checking':
-                return (
-                    <Checkbox.Group>
-                        {question.answers.map((e, i) => {
-                            return (
-                                <Checkbox value={i} key={i}>{e}</Checkbox>
-                            )
-                        })}
-                    </Checkbox.Group>
-                )
-            default:
-                return null
-        }
-    }
-
-
-    return (
-        <div>
-            <h3>Question: {question.question} ?</h3>
-            {questionOptions(question)}
-        </div>
-    );
-}
 
 
 export default Page;
